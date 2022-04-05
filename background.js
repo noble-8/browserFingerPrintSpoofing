40f0de5d6f8432ba5b7a2e18039927aa8c655196
Kalvinci <kalyanasundharam97@gmail.com>
2022-04-05 13:51:12 -0400

    user agent change implemented

--- background.js
+++ background.js
@@ -1,10 +1,1 @@
-// background.js
-
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
-
+chrome.storage.sync.set({ UA_CHANGED: false });
--- popup.js
+++ popup.js
@@ -1,23 +1,63 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+actionBtn = document.querySelector("#change-ua");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
+chrome.storage.sync.get("UA_CHANGED", ({ UA_CHANGED }) => {
+	actionBtn.textContent = UA_CHANGED ? "Stop" : "Change UA";
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+actionBtn.addEventListener("click", (event) => {
+	chrome.storage.sync.get("UA_CHANGED", ({ UA_CHANGED }) => {
+		userAgentChanged = UA_CHANGED;
+		const newValue = !UA_CHANGED;
+		chrome.storage.sync.set({ UA_CHANGED: newValue }, async () => {
+			let userAgent = "";
+			if (!newValue) {
+				userAgent = window.navigator.userAgent;
+				actionBtn.textContent = "Change UA";
+			} else {
+				userAgent = await getRandomUserAgent();
+				actionBtn.textContent = "Stop";
+			}
+			chrome.declarativeNetRequest.updateDynamicRules(
+				{
+					removeRuleIds: [1],
+					addRules: [
+						{
+							id: 1,
+							priority: 10,
+							action: {
+								type: "modifyHeaders",
+								requestHeaders: [
+									{
+										header: "user-agent",
+										operation: "set",
+										value: userAgent,
+									},
+								],
+							},
+							condition: {
+								urlFilter: "*",
+								resourceTypes: ["main_frame"],
+							},
+						},
+					],
+				},
+				() => {
+					reload();
+				}
+			);
+		});
+	});
+});
+
+async function reload() {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+	chrome.tabs.reload(tab.id);
+}
+
+async function getRandomUserAgent() {
+	const response = await fetch(
+		"https://browser-fingerprint-spoof.herokuapp.com/getRandomUserAgent"
+	);
+	let userAgent = await response.text();
+	return userAgent;
 }
