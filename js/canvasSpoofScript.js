55c9147f8b259de9eddf3614005790e29b8ec7d3
Akash <akash7nair@gmail.com>
2022-04-15 12:39:25 -0400

    add random canvas image generation

--- js/canvasSpoofScript.js
+++ js/canvasSpoofScript.js
@@ -1,15 +1,25 @@
 let canvas = document.createElement("canvas");
 canvas.id = "1337h4x0r";
-canvas.width = 1224;
-canvas.height = 768;
-canvas.style.zIndex = 8;
+const canvasWidth = 200;
+const canvasHeight = 200;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
 canvas.style.position = "absolute";
 canvas.style.border = "1px solid";
-const ctx = canvas.getContext("2d");
-
+var ctx = canvas.getContext("2d");
+for(let i=0;i<canvasHeight;i++){
+    for(let j=0;j<canvasWidth;j++){
+        //fill a pixel with rgb and brightness
+        ctx.fillStyle = "rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")"
+        ctx.fillRect(i,j,1,1);
+    }
+}
+//spoofed value generation
+ctx = canvas.getContext("2d");
+const dataURL = canvas.toDataURL();
 Object.defineProperty(HTMLCanvasElement.prototype, "toDataURL", {
 	get: () => () =>
-		"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAAA8CAYAAABIFuztAAABjklEQVR4nO3VoQ3CAABFwXYSEN2ABIMiFSQgOkFNEThGwJEOwAwMwQRMQEDhCZIFWACF6W9y+sxzr+g3zfZTTo718/XeX6a7dnnqrvPVYVYtzrfH+s4555z/8mLoAM455+N0A+Gcc24gnHPODYRzznm4GwjnnHMD4ZxzbiCcc87D3UA455wbCOeccwPhnHMe7gbCOefcQDjnnBsI55zzcDcQzjnnBsI559xAOOech7uBcM45NxDOOecGwjnnPNwNhHPOuYFwzjk3EM455+FuIJxzzg2Ec865gXDOOQ93A+Gcc24gnHPODYRzznm4GwjnnHMD4ZxzbiCcc87D3UA455wbCOeccwPhnHMe7gbCOefcQDjnnBsI55zzcDcQzjnnBsI559xAOOech7uBcM45NxDOOecGwjnnPNwNhHPOuYFwzjk3EM455+FuIJxzzg2Ec865gXDOOQ93A+Gcc24gnHPODYRzznm4GwjnnHMD4ZxzbiCcc87D3UA455wbCOeccwPhnHMe7gbCOef8L/8Cl9z3SWtDbpIAAAAASUVORK5CYII=",
+	dataURL,
 });
 
 Object.defineProperty(HTMLCanvasElement.prototype, "getContext", {
